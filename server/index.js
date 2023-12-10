24faa94f582b6eb60f011385e97301f7184cb7cb
AbhiSpeak <abhi0108jha@gmail.com>
2023-12-11 00:53:19 +0530

    modified the index file.

--- server/index.js
+++ server/index.js
@@ -1,8 +1,16 @@
 const express = require('express')
+const mongoose = require('mongoose')
+require('dotenv').config()
 const app = express()
+
+mongoose.connect(process.env.MONGO_URL)
 
 app.get('/test', (req, res) => {
     res.json('test ok')
+})
+
+app.get('/register', (req, res) => {
+
 })
 
 app.listen(4000);
