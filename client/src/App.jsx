4b5f874f65cb7e5b967f8f381b613d0095dabc95
AbhiSpeak <abhi0108jha@gmail.com>
2023-12-10 23:51:16 +0530

    new packages and modification of the app.

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,15 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
+import {BrowserRouter, Link, Route, Routes} from 'react-router-dom'
+import Register from './Pages/Register'
+import Home from './Pages/Home'
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <div className='bg-black '>test</div>
+    <BrowserRouter>
+        <Routes>
+        <Route path="/" element={<Home />} />
+          <Route path="/register" element={<Register />} />
+        </Routes>
+    </BrowserRouter>
   )
 }
 
